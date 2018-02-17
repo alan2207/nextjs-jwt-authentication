ea35449b8a65d74aa3c2806d68e74672f653a3e0
alan2207 <alan2207@live.com>
2018-02-17 20:34:46 +0100

    nextjs image added on index page

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import Layout from '../components/Layout';
 const Index = () => (
   <Layout title="Home">
     <h2 className="title is-2">Authentication with Next.js and JWT</h2>
+    <img src="/static/nextjs.jpg" />
     <p>A proof of concept app, demonstrating the authentication of Next.js application using JWT.</p>
   </Layout>
 );
