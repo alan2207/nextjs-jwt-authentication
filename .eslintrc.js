3a436a013f372b8a9185843d2d9dd826f7893cc4
alan2207 <alan2207@live.com>
2018-02-17 10:22:58 +0100

    es2017 support added

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
     },
     "extends": "eslint:recommended",
     "parserOptions": {
+        'ecmaVersion': 2017,
         "ecmaFeatures": {
             "experimentalObjectRestSpread": true,
             "jsx": true
